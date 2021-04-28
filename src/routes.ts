e99697d6b6657125100b9ac97e13738a7e1720d4
Deyvis Charles <davis.superheavy@gmail.com>
2021-04-28 07:07:00 -0300

    Criação da rota de autenticação de usuários

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import Home from './routes/home'
 import Blog from './routes/blog'
 import User from './routes/user'
+import Security from './routes/security'
 
 const routes = Router()
 
 routes.get('/', Home.index)
 routes.get('/blog', Blog.index)
 routes.post('/register', User.register)
+routes.post('/authenticate', Security.authenticate)
 
 export default routes
---
+++ src/routes/security.ts
@@ -0,0 +1,51 @@
+import { Request, Response} from 'express'
+import { PrismaClient } from '@prisma/client'
+import Bcrypt from 'bcrypt'
+import Verify from '../utils/verify'
+import Token from '../utils/token'
+
+class Security {
+    public async authenticate (req: Request, res: Response) {
+        const { email, password } = req.body
+
+        const prisma = new PrismaClient
+
+        const user: any = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+
+        if (Verify.isEmpyt(email)) {
+            res.status(400).send({
+                message: "O campo de email está vazio!"
+            })
+        } else if (Verify.notEmail(email)) {
+            res.status(400).send({
+                message: "Informe um email válido!"
+            })
+        } else if (!await user) {
+            res.status(401).send({
+                message: 'Email ou senha incorreto!'
+            })
+        } else if (Verify.isEmpyt(password)) {
+            res.status(400).send({
+                message: "O campo de senha está vazio!"
+            })
+        } else if (!await Bcrypt.compare(password, user.password)) {
+            res.status(401).send({
+                message: 'Email ou senha incorreto!'
+            })
+        } else {
+            delete user.password
+
+            res.send({
+                message: 'Acesso Autorizado!',
+                user: user,
+                token: Token.generate({id: user.id})
+            })
+        }
+    }
+}
+
+export default new Security
