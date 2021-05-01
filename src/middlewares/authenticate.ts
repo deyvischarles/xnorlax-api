ee08115aa2d07043b7069af0b6f42257c0e12873
Deyvis Charles <davis.superheavy@gmail.com>
2021-05-01 07:19:53 -0300

    Criação do middleware de autenticação

---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+export default function authenticate (req: Request, res: Response, next: NextFunction) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
+
+    const { authorization } = req.headers
+
+    if (!authorization) {
+        res.status(401).send({
+            message: 'Nenhum Token encontrado!'
+        })
+    } else {
+        const parts = authorization.split(' ')
+
+        if (parts.length != 2) {
+            res.status(401).send({
+                message: 'Token mau formatado!'
+            })
+        } else {
+            const [ scheme, token ] = parts
+    
+            if (!/^Bearer$/i.test(scheme)) {
+                res.status(401).send({
+                    message: 'Token desconhecido!'
+                })
+            } else if (!process.env.SECRET_KEY) {
+                res.status(401).send({
+                    message: 'Variável de ambiente ausente!'
+                })
+            } else {
+                const hash = process.env.SECRET_KEY
+
+                jwt.verify(token, hash, (err, decoded: any) => {
+                    if (err) {
+                        res.status(401).send({
+                            message: 'Token inválido!'
+                        })
+                    } else {
+                        req.userId = decoded.id
+                        return next()
+                    }
+                })
+            }
+        }
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 
+import Middleware from './middlewares/authenticate'
+
 import Home from './routes/home'
 import Blog from './routes/blog'
 import User from './routes/user'
@@ -8,7 +10,7 @@ import Security from './routes/security'
 const routes = Router()
 
 routes.get('/', Home.index)
-routes.get('/blog', Blog.index)
+routes.get('/blog', Middleware, Blog.index)
 routes.post('/register', User.register)
 routes.post('/authenticate', Security.authenticate)
 
