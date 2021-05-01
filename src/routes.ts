ce7ecb0df48617b17477ad655c3c87227a48ff79
Deyvis Charles <davis.superheavy@gmail.com>
2021-05-01 16:29:11 -0300

    novas feaures para rota de Usuários

--- src/routes.ts
+++ src/routes.ts
@@ -10,8 +10,17 @@ import Security from './routes/security'
 const routes = Router()
 
 routes.get('/', Home.index)
-routes.get('/blog', Middleware, Blog.index)
-routes.post('/register', User.register)
+routes.get('/blog', Blog.index)
+
 routes.post('/authenticate', Security.authenticate)
+
+routes.post('/register', User.register)
+routes.post('/users', Middleware, User.getUsers)
+routes.get('/user/:id', Middleware, User.getUser)
+routes.patch('/user/:id/name', Middleware, User.updateName)
+routes.patch('/user/:id/email', Middleware, User.updateEmail)
+routes.patch('/user/:id/password', Middleware, User.updatePassword)
+routes.delete('/user/:id', Middleware, User.deleteUser)
+
 
 export default routes
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -26,7 +26,7 @@ class User {
             res.status(400).send({
                 message: "Informe um email válido!"
             })
-        } else if (await prisma.user.findUnique({ where: {email: email} })) {
+        } else if (await prisma.user.findUnique({where: {email: email}})) {
             res.status(409).send({
                 message: "Este email já existe!"
             })
@@ -41,12 +41,12 @@ class User {
         } else if (Verify.notSecurity(password)) {
             res.status(400).send({
                 message: "Senha muito fraca!",
-                dicas: "Acrescente letras maiúculas, números, símbolos, e ou espaços vazios. Minimo 8 caracters"
+                dicas: "Acrescente letras maiúsculas, números, símbolos e/ou espaços vazios. Mínimo 8 caracteres"
             })
         } else {
             password = await Bcrypt.hash(password, 10)
 
-            await prisma.user.create({ data: { name, email, password } }).then((newUser: any) => {
+            await prisma.user.create({data: {name, email, password}}).then((newUser: any) => {
                 delete newUser.password
 
                 if (newUser) {
@@ -65,6 +65,218 @@ class User {
             }).catch((err) => {
                 res.status(400).send({
                     message: 'Erro ao cadastrar-se, tente novamente mais tarde!',
+                    debug: err
+                })
+            })
+        }
+    }
+
+    public async getUsers (req: Request, res: Response) {
+        const prisma = new PrismaClient
+
+        const users = await prisma.user.findMany().then((users) => {
+            if (users) {
+                res.send({
+                    acount: users.length,
+                    users: users
+                })
+            } else {
+                res.status(400).send({
+                    message: 'Não existe usuários cadastrados ainda!'
+                })
+            }
+        }).catch((err) => {
+            res.status(400).send({
+                message: 'Erro ao procurar usuários, tente novamente mais tarde!',
+                debug: err
+            })
+        })
+    }
+
+    public async getUser (req: Request, res: Response) {
+        const prisma = new PrismaClient
+
+        const user = await prisma.user.findUnique({where: {id: req.params.id}}).then((user: any) => {
+            delete user.password
+
+            if (user) {
+                res.send(user)
+            } else {
+                res.status(400).send({
+                    message: 'Usuário não encontrado!'
+                })
+            }
+        }).catch((err) => {
+            res.status(400).send({
+                message: 'Erro ao pesquisar usuário, tente novamente mais tarde!',
+                debug: err
+            })
+        })
+    }
+
+    public async updateName (req: Request, res: Response) {
+        const prisma = new PrismaClient
+
+        if (! await prisma.user.findUnique({where: {id: req.params.id}})) {
+            res.status(400).send({
+                message: "Usuário não encontrado!"
+            })
+        } else if (req.userId != req.params.id) {
+            res.send({
+                message: "Tentativa Negada!"
+            })
+        } else if (Verify.isEmpyt(req.body.name)) {
+            res.status(400).send({
+                message: "O campo de nome está vazio!"
+            })
+        } else if (Verify.notName(req.body.name)) {
+            res.status(400).send({
+                message: "Informe um nome válido!"
+            })
+        } else {
+            const userUpdate = await prisma.user.update({
+                where: { id: req.params.id },
+                data: { name: req.body.name }
+            }).then((userUpdate) => {
+                if (userUpdate) {
+                    res.send({
+                        message: 'Usuário atualizado com sucesso!'
+                    })
+                } else {
+                    res.status(400).send({
+                        message: 'Não foi possível atualizar usuário!'
+                    })
+                }
+            }).catch((err) => {
+                res.status(400).send({
+                    message: 'Erro ao atualizar usuário, tente novamente mais tarde!',
+                    debug: err
+                })
+            })
+        }
+    }
+
+    public async updateEmail (req: Request, res: Response) {
+        const prisma = new PrismaClient
+
+        if (! await prisma.user.findUnique({where: {id: req.params.id}})) {
+            res.status(400).send({
+                message: "Usuário não encontrado!"
+            })
+        } else if (req.userId != req.params.id) {
+            res.send({
+                message: "Tentativa Negada!"
+            })
+        } else if (Verify.isEmpyt(req.body.email)) {
+            res.status(400).send({
+                message: "O campo de email está vazio!"
+            })
+        } else if (Verify.notEmail(req.body.email)) {
+            res.status(400).send({
+                message: "Informe um email válido!"
+            })
+        } else if (await prisma.user.findUnique({where: {email: req.body.email}})) {
+            res.status(409).send({
+                message: "Este email não está disponivél!"
+            })
+        } else {
+            const userUpdate = await prisma.user.update({
+                where: { id: req.params.id },
+                data: { email: req.body.email }
+            }).then((userUpdate) => {
+                if (userUpdate) {
+                    res.send({
+                        message: 'Email atualizado com sucesso!'
+                    })
+                } else {
+                    res.status(400).send({
+                        message: 'Não foi possível atualizar o email!'
+                    })
+                }
+            }).catch((err) => {
+                res.status(400).send({
+                    message: 'Erro ao atualizar email, tente novamente mais tarde!',
+                    debug: err
+                })
+            })
+        }
+    }
+
+    public async updatePassword (req: Request, res: Response) {
+        const prisma = new PrismaClient
+
+        if (! await prisma.user.findUnique({where: {id: req.params.id}})) {
+            res.status(400).send({
+                message: "Usuário não encontrado!"
+            })
+        } else if (req.userId != req.params.id) {
+            res.send({
+                message: "Tentativa Negada!"
+            })
+        } else if (Verify.isEmpyt(req.body.password)) {
+            res.status(400).send({
+                message: "O campo de senha está vazio!"
+            })
+        } else if (Verify.minLength(req.body.password, 8)) {
+            res.status(400).send({
+                message: "Senha muito curta!"
+            })
+        } else if (Verify.notSecurity(req.body.password)) {
+            res.status(400).send({
+                message: "Senha muito Fraca!",
+                dicas: "Acrescente letras maiúsculas, números, símbolos e/ou espaços vazios. Mínimo 8 caracteres"
+            })
+        } else {
+            const password = await Bcrypt.hash(req.body.password, 10)
+
+            const userUpdate = await prisma.user.update({
+                where: {id: req.params.id},
+                data: {password: password}
+            }).then((userUpdate) => {
+                if (userUpdate) {
+                    res.send({
+                        message: 'Senha atualizada com sucesso!'
+                    })
+                } else {
+                    res.status(400).send({
+                        message: 'Não foi possível atualizar a senha!'
+                    })
+                }
+            }).catch((err) => {
+                res.status(400).send({
+                    message: 'Erro ao atualizar senha, tente novamente mais tarde!',
+                    debug: err
+                })
+            })
+        }
+    }
+
+    public async deleteUser (req: Request, res: Response) {
+        const prisma = new PrismaClient
+
+        if (! await prisma.user.findUnique({where: {id: req.params.id}})) {
+            res.status(400).send({
+                message: 'Usuário não encontrado!'
+            })
+        } else if (req.userId != req.params.id) {
+            res.send({
+                message: "Tentativa Negada!"
+            })
+        } else {
+            const userDelete = await prisma.user.delete({where: {id: req.params.id}}).then((userDelete) => {
+                if (userDelete) {
+                    res.send({
+                        message: 'Usuário apagado com sucesso!',
+                        userId: req.userId
+                    })
+                } else {
+                    res.status(400).send({
+                        message: 'Não foi possível pagar este usuário!'
+                    })
+                }
+            }).catch((err) => {
+                res.status(400).send({
+                    message: 'Erro ao apagar usuário, tente novamente mais tarde!',
                     debug: err
                 })
             })
