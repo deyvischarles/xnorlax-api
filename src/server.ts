556cf6a5b5f6ff031b690666fbda588c9f7a886e
Deyvis Charles <davis.superheavy@gmail.com>
2021-05-03 15:11:27 -0300

    Implementação da verificação que força o uso do protocolo https em produção

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,16 @@ import routes from './routes'
 
 const app = express()
 
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https') {
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        } else {
+            next()
+        }
+    })
+}
+
 app.use(json())
 app.use(routes)
 
