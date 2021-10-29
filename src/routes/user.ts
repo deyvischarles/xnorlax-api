4ac3b2fe1777441ee4e8d1e203a00630c924a628
Super Heavy <user@machine.localdomain>
2021-10-29 16:32:11 -0300

    Melhorias na rota de Usuários

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,8 +8,8 @@ class User {
     public async register (req: Request, res: Response) {
         const prisma = new PrismaClient
 
-        let { name, email, password } = req.body
-        let level = 'noob'
+        let { name, email, password, passwordConfirmation } = req.body
+        let level = 1
 
         if (Verify.isEmpyt(name)) {
             res.status(400).send({
@@ -37,19 +37,27 @@ class User {
             })
         } else if (Verify.minLength(password, 8)) {
             res.status(400).send({
-                message: "Senha muito curta!"
+                message: "Senha muito curta! Mínimo 8 caracteres."
             })
         } else if (Verify.notSecurity(password)) {
             res.status(400).send({
                 message: "Senha muito fraca!",
-                dicas: "Acrescente letras maiúsculas, números, símbolos e/ou espaços vazios. Mínimo 8 caracteres"
+                dicas: "Acrescente letras maiúsculas, números, símbolos e/ou espaços em branco."
+            })
+        } else if (Verify.isEmpyt(passwordConfirmation)) {
+            res.status(400).send({
+                message: "Confirme a senha!"
+            })
+        } else if (passwordConfirmation !== password) {
+            res.status(400).send({
+                message: "As senhas não coincidem!"
             })
         } else {
             password = await Bcrypt.hash(password, 10)
 
             await prisma.user.findMany().then((users) => {
                 if (users.length === 0) {
-                    level = 'founder'
+                    level = 6
                 }
             })
 
@@ -71,7 +79,7 @@ class User {
 
             }).catch((err) => {
                 res.status(400).send({
-                    message: 'Erro ao cadastrar-se, tente novamente mais tarde!',
+                    message: 'Erro ao cadastrar-se, tente novamente ou reporte-nos!',
                     debug: err
                 })
             })
@@ -93,7 +101,7 @@ class User {
                 })
             } else {
                 res.status(400).send({
-                    message: 'Não existe usuários cadastrados ainda!'
+                    message: 'Ainda não há usuários cadastrados!'
                 })
             }
         }).catch((err) => {
